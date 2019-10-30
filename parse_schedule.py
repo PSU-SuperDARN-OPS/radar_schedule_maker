281a8274ffbab5b95aab01989485b8de8dfa9d22
bgklug <bgklug@alaska.edu>
2019-10-29 17:57:50 -0800

    Separate operations from the rest of the file

--- parse_schedule.py
+++ parse_schedule.py
@@ -1,4 +1,20 @@
 
 with open('sample.txt', 'r') as file:
-    file_contents = file.read()
-    print(file_contents)
+    date = file.readline()
+
+    rest_of_file = file.read()
+
+operations = rest_of_file.split('#')[0]
+common_time = rest_of_file.split('#')[1]
+discretionary_time = rest_of_file.split('#')[2]
+special_time = rest_of_file.split('#')[3]
+if len(rest_of_file.split('#')) > 4:
+    notes = rest_of_file.split('#')[4]
+else:
+    notes = "No notes"
+
+print("Schedule date: " + date)
+print("Scheduled operations")
+print(operations)
+print("Notes:")
+print(notes)

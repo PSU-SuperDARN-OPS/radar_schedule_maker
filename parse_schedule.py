a92eaa94176d746ddc8f5f4692f6ef5e776ec354
bgklug <bgklug@alaska.edu>
2019-10-30 14:42:56 -0800

    Refactored, start note copying

--- parse_schedule.py
+++ parse_schedule.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # Minimum requirements are Python 3.6
 from datetime import datetime
+import re
 
-class Schedule_Parser:
+
+class parse_schedule:
     """A class that parses the SuperDARN scheduling files and returns a generic schedule for the radars"""
     def __init__(self, filename):
         self.filename = filename
@@ -20,9 +22,12 @@ class Schedule_Parser:
 
         operations = rest_of_file.split('#')[0].rstrip()
         self.operations = operations.split('\n')
+
+        # Currently don't care about the following lines, discarding to get to the notes section
         common_time = rest_of_file.split('#')[1]
         discretionary_time = rest_of_file.split('#')[2]
         special_time = rest_of_file.split('#')[3]
+
         if len(rest_of_file.split('#')) > 4:
             self.notes = rest_of_file.split('#')[4]
         else:
@@ -32,6 +37,10 @@ class Schedule_Parser:
         self.month = date.month
         self.year = date.year
 
+    def get_notes(self):
+        note_str = re.search(r'Note [A-Z]', self.notes, re.M)
+        print(note_str.group())
+
     def generate_schedule(self):
         entry = {
             'Start Day' : 0,
@@ -39,6 +48,7 @@ class Schedule_Parser:
             'Stop Day'  : 0,
             'Stop Hour' : 0,
             'Duration'  : 0,
+            'Time String': '',
             'Mode'  : 'Common Time'
         }
 
@@ -54,18 +64,28 @@ class Schedule_Parser:
                                 + (entry['Stop Hour'] - entry['Start Hour']) * 60
             self.total_duration += entry['Duration']
 
-            entry['Mode'] = operation[2]
+            entry['Time String'] = f"{self.year} {self.month:02} " \
+                f"{entry['Start Day']:02} {entry['Start Hour']:02} 00 {entry['Duration']:6}"
+
+            entry['Mode'] = operation[2].split()[0]
+            if entry['Mode'] == 'Special':
+                entry['Mode'] += " (" + operation[2].split("(see ")[1]
 
             self.schedule.append(entry.copy())
 
+    def get_schedule(self):
+        return self.schedule
+
     def print_schedule(self):
         for line in self.schedule:
-            print(f"{self.year} {self.month:02} {line['Start Day']:02} {line['Start Hour']:02} 00 {line['Duration']:6} {line['Mode']}")
-        print(f"Monthly total: {self.total_duration}, Should be {self.schedule[len(self.schedule)-1]['Stop Day']*24*60}")
+            print(f"{line['Time String']} {line['Mode']}")
+        print(f"Monthly total: {self.total_duration}, "
+              f"Should be {self.schedule[len(self.schedule)-1]['Stop Day']*24*60}")
 
 
 if __name__ == '__main__':
-    parser = Schedule_Parser('sample_notes.txt')
+    parser = parse_schedule('sample_notes.txt')
     parser.read_schedule()
     parser.generate_schedule()
     parser.print_schedule()
+    parser.get_notes()

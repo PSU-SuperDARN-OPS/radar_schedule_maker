213aff0246bc7435a294143f7c898db6dbe73fc1
bgklug <bgklug@alaska.edu>
2019-10-31 15:38:52 -0800

    Add more test schedules, update paths

--- tests/test_scheduleparser.py
+++ tests/test_scheduleparser.py
@@ -4,20 +4,20 @@ import schedule_parser
 
 
 @pytest.fixture
-def good_schedule():
+def good_schedule_legacy():
     '''Returns a ScheduleParser instance with a good schedule'''
-    parser = schedule_parser.ScheduleParser('tests/test_schedules/good_schedule.txt')
+    parser = schedule_parser.ScheduleParser('tests/test_schedules/good_schedule_no_notes_legacy.txt')
     parser.read_schedule()
     return parser
 
 
-def test_notes_dont_exist(good_schedule):
-    assert good_schedule.notes_exist is False
+def test_notes_dont_exist(good_schedule_legacy):
+    assert good_schedule_legacy.notes_exist is False
 
 
-def test_month(good_schedule):
-    assert good_schedule.month == 9
+def test_month(good_schedule_legacy):
+    assert good_schedule_legacy.month == 9
 
 
-def test_year(good_schedule):
-    assert good_schedule.year == 2019
+def test_year(good_schedule_legacy):
+    assert good_schedule_legacy.year == 2019

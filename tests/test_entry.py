3bacfdf31c059f53bb845a17dd67a28e6cc9196a
bgklug <bgklug@alaska.edu>
2019-10-31 15:50:13 -0800

    Make default tests

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -1,3 +1,21 @@
 import pytest
 
 import entry
+
+
+@pytest.fixture
+def default_entry():
+    return entry.Entry()
+
+
+@pytest.fixture
+def good_entry():
+    return entry.Entry(2019, 1, 1, 0, 10, 24, 'Common')
+
+
+def test_default_entry_year(default_entry):
+    assert default_entry.get_year() == 0
+
+
+def test_default_entry_month(default_entry):
+    assert default_entry.get_month() == 0

55ed8f64e12968317821e89e99199a4334f15693
bgklug <bgklug@alaska.edu>
2019-11-20 16:33:47 -0900

    Added test for parsing the schedule and operations

--- tests/test_scheduleparser.py
+++ tests/test_scheduleparser.py
@@ -24,19 +24,36 @@ def test_notes_dont_exist_legacy(good_schedule_legacy):
     assert good_schedule_legacy.notes_exist is False
 
 
+# Test for set_date
 def test_month(good_schedule_legacy):
     good_schedule_legacy.set_date()
     assert good_schedule_legacy.month == 9
 
-
+# Test for set_date
 def test_year(good_schedule_legacy):
     good_schedule_legacy.set_date()
     assert good_schedule_legacy.year == 2019
 
 
+def test_read_schedule(good_schedule):
+    assert good_schedule.raw_date == "December 2019"
+    assert good_schedule.raw_operations[0] == "01:00    03:06    Common Time (1-min)"
+    assert good_schedule.raw_notes[0] == "Note A: NORMALSCAN - This is a spacecraft working group request to support"
+
+
 def test_notes(good_schedule, monkeypatch):
     # Override the Python built-in input method
     monkeypatch.setattr('builtins.input', lambda x: 'y')
+
     good_schedule.get_notes()
     assert good_schedule.notes['Note A'] == "NORMALSCAN"
+    assert good_schedule.notes['Note C'] == "RBSPSCAN"
 
+
+def test_operation(good_schedule, monkeypatch):
+    # Override the Python built-in input method
+    monkeypatch.setattr('builtins.input', lambda x: 'y')
+
+    good_schedule.get_notes()
+    good_schedule.get_operation()
+    assert good_schedule.print_schedule() == ""

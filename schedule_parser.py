95548d9d69df85cc67afddfb9c801efdb3c5d006
bgklug <bgklug@alaska.edu>
2019-11-20 16:14:21 -0900

    Refactored parser function to do only one thing

--- schedule_parser.py
+++ schedule_parser.py
@@ -5,13 +5,22 @@ from schedule import Schedule
 
 
 class ScheduleParser:
-    """A class that parses the SuperDARN scheduling files and returns a generic schedule for the radars"""
+    """
+    A class that parses the SuperDARN scheduling files and returns a generic schedule for the radars.
+
+
+    """
     def __init__(self, filename):
         self.filename = filename
+
+        self.raw_date = ""
+        self.raw_operations = []
+        self.raw_notes = []
+
         self.year = 2000
         self.month = 1
-        self.operations = []
-        self.note_str = ''
+        # self.operations = []
+        # self.note_str = ""
         self.notes = {}
         self.notes_exist = False
         self.total_duration = 0
@@ -19,61 +28,42 @@ class ScheduleParser:
 
     def read_schedule(self):
         with open(self.filename, 'r') as file:
-            date_str = file.readline().rstrip()
-            rest_of_file = file.read()
+            self.raw_date = file.readline().rstrip()
 
-        operations = rest_of_file.split('#')[0].rstrip()
-        self.operations = operations.split('\n')
+            for line in file:
+                if line[0] == '#':
+                    pass  # Ignore comment
+                elif line[0] == '0' or line[0] == '1' or line[0] == '2' or line[0] == '3':
+                    self.raw_operations.append(line.rstrip())
+                elif "Note" in line:
+                    self.raw_notes.append(line.rstrip())
+                    self.notes_exist = True
+                else:
+                    # Ignore line
+                    pass
 
-        # Currently don't care about the following lines, discarding to get to the notes section
-        common_time = rest_of_file.split('#')[1]
-        discretionary_time = rest_of_file.split('#')[2]
-        special_time = rest_of_file.split('#')[3]
-
-        if len(rest_of_file.split('#')) > 4:
-            self.note_str = rest_of_file.split('#')[4]
-            self.notes_exist = True
-        else:
-            self.notes_exist = False
-
-        self.set_date(date_str)
-
-    def set_date(self, date_str):
-        date = datetime.strptime(date_str, "%B %Y")
+    def set_date(self):
+        date = datetime.strptime(self.raw_date, "%B %Y")
         self.month = date.month
         self.year = date.year
         self.schedule.set_month(self.month)
         self.schedule.set_year(self.year)
 
-    def __get_notes(self):
-        note_sections = self.note_str.split('Note ')[1:]
-
+    def get_notes(self):
         i = 0
-        for note in note_sections:
-            correct_mode = input(f"If '{note.split()[1]}' is the correct mode for Note {chr(ord('A') + i)}, "
+        for note in self.raw_notes:
+            correct_mode = input(f"If '{note.split()[2]}' is the correct mode for Note {chr(ord('A') + i)}, "
                                  f"enter 'y'. Else enter the correct mode: ")
+
             if correct_mode == 'y':
-                self.notes.update({f"Note {chr(ord('A') + i)})": note.split()[1]})
+                self.notes.update({f"Note {chr(ord('A') + i)}": note.split()[2]})
             else:
-                self.notes.update({f"Note {chr(ord('A') + i)})": correct_mode})
+                self.notes.update({f"Note {chr(ord('A') + i)}": correct_mode})
             i += 1
 
-    def format_schedule(self):
-        entry = {
-            'Start Day': 0,
-            'Start Hour': 0,
-            'Stop Day': 0,
-            'Stop Hour': 0,
-            'Duration': 0,
-            'Time String': '',
-            'Mode': 'Common Time'
-        }
-
-        if self.notes_exist:
-            self.__get_notes()
-
-        for op in range(len(self.operations)):
-            operation = self.operations[op].split("    ")
+    def get_operation(self):
+        for op in range(len(self.raw_operations)):
+            operation = self.raw_operations[op].split("    ")
             start_time = operation[0]
             stop_time = operation[1]
             mode_and_note = operation[2]
@@ -85,9 +75,17 @@ class ScheduleParser:
 
             mode = mode_and_note.split()[0]
             if mode == 'Special':
-                mode += ":" + self.notes[mode_and_note.split("(see ")[1]]
+                mode += ":" + self.notes[mode_and_note.split("(see ")[1][:-1]]
 
             self.schedule.add_entry(start_day, start_hour, stop_day, stop_hour, mode)
+
+    def format_schedule(self):
+        self.set_date()
+
+        if self.notes_exist:
+            self.get_notes()
+
+        self.get_operation()
 
         self.total_duration = self.schedule.get_duration()
 

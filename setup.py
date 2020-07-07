0a03eff84c0ed808d2ee611e8a090c2ef4266d9a
bgklug <bgklug@alaska.edu>
2020-07-06 16:52:45 -0800

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='radar_schedule_maker',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/UAF-SuperDARN-OPS/radar_schedule_maker',
+    license='',
+    author='bgklug',
+    author_email='',
+    description=''
+)
